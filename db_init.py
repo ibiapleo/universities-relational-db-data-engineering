7adceab01241fcde40b7cd72271503628978de4c
ibiapleo <leonardojose159357@gmail.com>
2026-03-20 19:24:49 -0300

    style: apply Black formatting to codebase

--- db_init.py
+++ db_init.py
@@ -4,8 +4,9 @@ import uuid
 
 Base = declarative_base()
 
+
 class University(Base):
-    __tablename__ = 'universities'
+    __tablename__ = "universities"
     id = Column(String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
     name = Column(String, nullable=False)
     country = Column(String, nullable=False)
@@ -14,19 +15,22 @@ class University(Base):
     domains = relationship("Domain", back_populates="university")
     web_pages = relationship("WebPage", back_populates="university")
 
+
 class Domain(Base):
-    __tablename__ = 'university_domains'
+    __tablename__ = "university_domains"
     id = Column(Integer, primary_key=True)
-    university_id = Column(Integer, ForeignKey('universities.id'))
+    university_id = Column(Integer, ForeignKey("universities.id"))
     domain = Column(String)
     university = relationship("University", back_populates="domains")
 
+
 class WebPage(Base):
-    __tablename__ = 'university_webpages'
+    __tablename__ = "university_webpages"
     id = Column(Integer, primary_key=True)
-    university_id = Column(Integer, ForeignKey('universities.id'))
+    university_id = Column(Integer, ForeignKey("universities.id"))
     web_page = Column(String)
     university = relationship("University", back_populates="web_pages")
 
-engine = create_engine('sqlite:///unis.db')
+
+engine = create_engine("sqlite:///unis.db")
 Base.metadata.create_all(engine)
